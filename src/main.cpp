1608ebd90f2c6618ae1464b79ebf215aff55c41b
zhaobing.lv <zhaobing.lv@megatronix.co>
2021-03-11 12:23:52 +0800

    add C++ class object deserialize

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,16 @@ using json = nlohmann::json;
 int main()
 {
     Person p = {"张三", 20};
+    
+    //serialize
     json j = p;
-
     std::cout << j << std::endl;
+
+    //change property
+    j["height"] = 180.0f;
+    
+    //deserialize
+    p = j.get<Person>();
+    std::cout << "height: "<< p.getHeight() << std::endl;
     
 }
